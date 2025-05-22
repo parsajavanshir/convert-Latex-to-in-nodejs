9bb4459157eaecd37fac9cd74a0beb72aa3e5fe3
Parsa Javanshir <79544911+parsajavanshir@users.noreply.github.com>
2025-05-22 16:10:40 +0330

    Fix: Body-parser , middleware , route and error handling

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express=require("express")
-const app=express()
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
-const text=require("./routes/text")
-app.get("/",(req,res,next)=>{
-    res.send("Live")
-})
+const text = require("./routes/text");
 
-app.use('/text',text)
-var port=3000
+app.get("/", (req, res) => {
+    res.send("Live");
+});
+
+app.use('/text', text);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const port = 3000;
 app.listen(port, () => console.log(`Devserver live on http://localhost:${port}`));
